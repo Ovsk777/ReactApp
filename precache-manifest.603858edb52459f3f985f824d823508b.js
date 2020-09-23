df40c7ca2eda3f3f2af1c67395420b75c1cefb80
Ovsk777 <abhishekpal92@yahoo.co.in>
2020-09-23 23:28:53 +0530

    Updates

---
+++ precache-manifest.603858edb52459f3f985f824d823508b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad7865b15d6309abb37469c1e27cbbe",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "e72a9fdc9a0d4be30910",
+    "url": "/ReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1926cfe0a695550ff462",
+    "url": "/ReactApp/static/js/2.3385ad01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.3385ad01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72a9fdc9a0d4be30910",
+    "url": "/ReactApp/static/js/main.c22a23bb.chunk.js"
+  },
+  {
+    "revision": "4cd5e51727c98a34dc07",
+    "url": "/ReactApp/static/js/runtime-main.2d7a9c30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
